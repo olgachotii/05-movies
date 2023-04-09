c032820356c42b4e7bd2da294aa8956acacde348
olgachotii <olgachoty@gmail.com>
2023-04-09 13:47:00 +0200

    2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/05-movies">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
